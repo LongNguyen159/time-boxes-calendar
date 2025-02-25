eea4b73bb6a1ff42a2abe5e29e34b55e6602b6bc
long <nguyenthanhlong@morenergy.net>
2025-02-25 10:23:32 +0100

    build: install dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeekService, MonthService, WorkWeekService, DayService, AgendaService } from '@syncfusion/ej2-angular-schedule';
+import { ScheduleModule, View } from '@syncfusion/ej2-angular-schedule'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ScheduleModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService],
 })
 export class AppComponent {
   title = 'time-boxes-calendar';
